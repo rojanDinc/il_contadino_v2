90d2819c755b48404360156c465a505f06a98db3
rojan-dinc <rojan.dinc@schibsted.com>
2021-06-25 16:56:43 +0200

    Add copy-webpack-plugin configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/script.js',
@@ -26,6 +27,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html")
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "CNAME"), to: path.resolve(__dirname, 'dist') }
+      ]
     })
   ],
   devServer: {
